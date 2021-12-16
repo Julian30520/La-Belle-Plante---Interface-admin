d0f0029a261b6e3f69a1943d63a6ce9122fdc01a
Jérémy Bojko macMini <jeremy.bojko@protonmail.com>
2021-12-16 09:34:00 +0100

    4.0 - filter-side-nav before categories

--- src/app/pages/page-accueil/page-accueil.component.ts
+++ src/app/pages/page-accueil/page-accueil.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import * as _ from 'underscore';
+import { list_products } from '../../data';
 
 @Component({
   selector: 'app-page-accueil',
@@ -6,10 +8,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./page-accueil.component.scss']
 })
 export class PageAccueilComponent implements OnInit {
+  listData = list_products;
 
   constructor() { }
 
   ngOnInit(): void {
+    let even = _.find([1, 2, 3, 4, 5, 6], function(num){ return num % 2 == 0; });
+    console.log(even); 
   }
 
 }
