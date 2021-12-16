b7ea0587e5e488359face3e5225bd84d2f2e0c83
Jérémy Bojko macMini <jeremy.bojko@protonmail.com>
2021-12-16 11:28:32 +0100

    4.1 - filter side bar + caterogies

--- src/app/pages/page-accueil/page-accueil.component.ts
+++ src/app/pages/page-accueil/page-accueil.component.ts
@@ -8,13 +8,37 @@ import { list_products } from '../../data';
   styleUrls: ['./page-accueil.component.scss']
 })
 export class PageAccueilComponent implements OnInit {
-  listData = list_products;
+  public listData = list_products;
+  public listCategoriesFilter: string[];
 
-  constructor() { }
+  constructor() {
+    this.listCategoriesFilter = [];
+   }
 
   ngOnInit(): void {
     let even = _.find([1, 2, 3, 4, 5, 6], function(num){ return num % 2 == 0; });
     console.log(even); 
+
+    /**
+     * Technique avec Underscore JS pour recupérer les catégories uniques de nos plantes
+     */
+    const listAllCategories = this.listData.map(product => product.product_breadcrumb_label);
+    console.log(listAllCategories);
+    
+    const listUniqCategories = _.uniq(listAllCategories) 
+    console.log(listUniqCategories);
+    
+
+    /**
+     * Technique native JS pour recupérer les catégories uniques de nos plantes
+     */
+
+    const listUniqJsCategories = [...new Set(listAllCategories)];
+    console.log(listUniqJsCategories);
+
+    this.listCategoriesFilter = listUniqJsCategories;
+
+
   }
 
 }
