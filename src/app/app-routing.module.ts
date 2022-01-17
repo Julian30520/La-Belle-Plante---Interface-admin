ef863e099aecb422fe267e4b979008a68abb9699
Jérémy Bojko macMini <jeremy.bojko@protonmail.com>
2022-01-17 12:09:23 +0100

    9 - module account+ pages + routing + lazy loading

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,10 +4,16 @@ import { PageAccueilComponent } from './pages/page-accueil/page-accueil.componen
 import { PageDetailsComponent } from './pages/page-details/page-details.component';
 import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.component';
 
+//Lazy Loading du module account à implémenter dans ce fichier
+
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', component: PageAccueilComponent },
   { path: 'details', component: PageDetailsComponent },
+  { path: 'account', 
+    loadChildren: () => import('./modules/account/account.module')
+    .then(m => m.AccountModule)
+  },
   { path: '**', component: PageNotFoundComponent }
 ];
 
