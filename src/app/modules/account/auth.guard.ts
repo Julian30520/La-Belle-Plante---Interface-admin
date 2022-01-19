8745d80bb376558f7a798c4933b69909f658372b
Jérémy Bojko macMini <jeremy.bojko@protonmail.com>
2022-01-19 16:39:22 +0100

    16 - guard + token expiration date

--- src/app/modules/account/auth.guard.ts
+++ src/app/modules/account/auth.guard.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
+import jwt_decode from 'jwt-decode';
 
 @Injectable({
   providedIn: 'root'
@@ -17,8 +18,22 @@ export class AuthGuard implements CanActivate {
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
       
       const token = localStorage.getItem(this.tokenKey);
-
+      
       if(token) {
+        const decodedToken = jwt_decode<any>(token);
+        
+        console.log('decodedToken : ', decodedToken);
+        
+        if(decodedToken.exp) {
+          console.log('Date d\'exp decodedToken : ', decodedToken.exp);
+          const dateExp = new Date(decodedToken.exp * 1000);
+          if(new Date() >= dateExp) {
+            // le token a expiré, je n'autorise pas l'accès
+            this.router.navigate(['account/signin']);
+            return false;
+          }
+        }
+        
         console.log("C'est ok ! ")
         return true;
       } else {
